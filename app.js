554d45b3fcdaa77c5d1e035a5a7c6dce512d187c
Tae-yoon <yeotaeyoon@gmail.com>
2019-03-17 23:23:23 +0900

    refactor ES5 codes into ES2015 codes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var wordCount = 10;
-  var guessCount = 4;
-  var password = '';
+  const wordCount = 10;
+  let guessCount = 4;
+  let password = '';
 
   var start = document.getElementById('start');
   start.addEventListener('click', function() {
@@ -10,22 +10,23 @@ document.addEventListener('DOMContentLoaded', function() {
     startGame();
   });
 
-  function toggleClasses(element) {
-    for (var i = 1; i < arguments.length; i++) {
-      element.classList.toggle(arguments[i]);
+  function toggleClasses(element, ...classes) {
+    for (let i = 0; i < classes.length; i++) {
+      element.classList.toggle(classes[i]);
     }
   }
 
   function startGame() {
     // get random words and append them to the DOM
-    var wordList = document.getElementById('word-list');
+    const wordList = document.getElementById('word-list');
     // 'words' variable is from words.js
-    var randomWords = getRandomValues(words, wordCount); // eslint-disable-line no-undef
-    randomWords.forEach(function(word) {
-      var li = document.createElement('li');
+    const randomWords = getRandomValues(words, wordCount); // eslint-disable-line no-undef
+    
+    randomWords.forEach( word => {
+      let li = document.createElement('li');
       li.innerText = word;
       wordList.appendChild(li);
-    });
+    })
 
     // set a secret password and the guess count display
     password = getRandomValues(randomWords, 1)[0];
@@ -35,20 +36,16 @@ document.addEventListener('DOMContentLoaded', function() {
     wordList.addEventListener('click', updateGame);
   }
 
-  function getRandomValues(array, numberOfVals) {
-    return shuffle(array).slice(0, numberOfVals);
-  }
+  let getRandomValues = (array, numberOfVals) => shuffle(array).slice(0, numberOfVals);
 
   function shuffle(array) {
-    var arrayCopy = array.slice();
-    for (var idx1 = arrayCopy.length - 1; idx1 > 0; idx1--) {
+    let arrayCopy = array.slice();
+    for (let idx1 = arrayCopy.length - 1; idx1 > 0; idx1--) {
       // generate a random index between 0 and idx1 (inclusive)
-      var idx2 = Math.floor(Math.random() * (idx1 + 1));
+      let idx2 = Math.floor(Math.random() * (idx1 + 1));
 
       // swap elements at idx1 and idx2
-      var temp = arrayCopy[idx1];
-      arrayCopy[idx1] = arrayCopy[idx2];
-      arrayCopy[idx2] = temp;
+      [arrayCopy[idx1], arrayCopy[idx2]] = [arrayCopy[idx2], arrayCopy[idx1]]; 
     }
     return arrayCopy;
   }
@@ -56,16 +53,16 @@ document.addEventListener('DOMContentLoaded', function() {
   function setGuessCount(newCount) {
     guessCount = newCount;
     document.getElementById('guesses-remaining').innerText =
-      'Guesses remaining: ' + guessCount + '.';
+      `Guesses remaining: ${guessCount}.`;
   }
 
   function updateGame(e) {
     if (e.target.tagName === 'LI' && !e.target.classList.contains('disabled')) {
       // grab guessed word, check it against password, update view
-      var guess = e.target.innerText;
-      var similarityScore = compareWords(guess, password);
+      let guess = e.target.innerText;
+      let similarityScore = compareWords(guess, password);
       e.target.classList.add('disabled');
-      e.target.innerText = guess + ' --> Matching Letters: ' + similarityScore;
+      e.target.innerText = `${guess} --> Matching Letters: ${similarityScore}`;
       setGuessCount(guessCount - 1);
 
       // check whether the game is over
@@ -83,8 +80,8 @@ document.addEventListener('DOMContentLoaded', function() {
     if (word1.length !== word2.length) {
       throw 'Words must have the same length';
     }
-    var count = 0;
-    for (var i = 0; i < word1.length; i++) {
+    let count = 0;
+    for (let i = 0; i < word1.length; i++) {
       if (word1[i] === word2[i]) count++;
     }
     return count;
